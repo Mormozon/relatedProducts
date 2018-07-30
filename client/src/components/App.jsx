4724eaeec8bbd9359e75485f8fe1b0ef192f71dd
askim90 <lkraussl@gmail.com>
2018-07-30 01:35:18 -0700

    moved dummy data to separate file

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,64 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Product from './Product.jsx';
 import styles from './css/app.css';
+import { example } from '../dummyData.js';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      products: [
-        {
-         pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
-          name: 'Mofuu Mofuu Tippy Plushie 1',
-          averageRating: 4.6,
-          reviews: 12345,
-          price: 100.00,
-        },
-        {
-          pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
-          name: 'Mofuu Mofuu Tippy Plushie 2',
-          averageRating: 3.4,
-          reviews: 12345,
-          price: 100.00,
-        },
-        {
-          pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
-          name: 'Mofuu Mofuu Tippy Plushie 3',
-          averageRating: 2.7,
-          reviews: 12345,
-          price: 100.00,
-        },
-        {
-          pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
-          name: 'Mofuu Mofuu Tippy Plushie 4',
-          averageRating: 1.3,
-          reviews: 12345,
-          price: 100.00,
-        },
-        {
-          pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
-          name: 'Mofuu Mofuu Tippy Plushie 5',
-          averageRating: 0.6,
-          reviews: 12345,
-          price: 100.00,
-        },
-        {
-          pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
-          name: 'Mofuu Mofuu Tippy Plushie 6',
-          averageRating: 4.8,
-          reviews: 12345,
-          price: 100.00,
-        },
-        {
-          pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
-          name: 'Mofuu Mofuu Tippy Plushie 7',
-          averageRating: 4.2,
-          reviews: 12345,
-          price: 100.00,
-        },
-      ],
+      products: [],
     }
     
+  }
+
+  componentDidMount() {
+    this.setState({ products: example });
   }
 
   render() {
---
+++ client/src/dummyData.js
@@ -0,0 +1,53 @@
+module.exports = {
+  example: [
+    {
+     pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
+      name: 'Mofuu Mofuu Tippy Plushie 1',
+      averageRating: 4.6,
+      reviews: 12345,
+      price: 100.00,
+    },
+    {
+      pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
+      name: 'Mofuu Mofuu Tippy Plushie 2',
+      averageRating: 3.4,
+      reviews: 12345,
+      price: 100.00,
+    },
+    {
+      pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
+      name: 'Mofuu Mofuu Tippy Plushie 3',
+      averageRating: 2.7,
+      reviews: 12345,
+      price: 100.00,
+    },
+    {
+      pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
+      name: 'Mofuu Mofuu Tippy Plushie 4',
+      averageRating: 1.3,
+      reviews: 12345,
+      price: 100.00,
+    },
+    {
+      pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
+      name: 'Mofuu Mofuu Tippy Plushie 5',
+      averageRating: 0.6,
+      reviews: 12345,
+      price: 100.00,
+    },
+    {
+      pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
+      name: 'Mofuu Mofuu Tippy Plushie 6',
+      averageRating: 4.8,
+      reviews: 12345,
+      price: 100.00,
+    },
+    {
+      pictureURL: "https://ae01.alicdn.com/kf/HTB1I619lsuYBuNkSmRyq6AA3pXar/candice-guo-Super-cute-plush-toy-lovely-Tippy-rabbit-is-the-order-a-rabbit-soft-stuffed.jpg_220x220.jpg",
+      name: 'Mofuu Mofuu Tippy Plushie 7',
+      averageRating: 4.2,
+      reviews: 12345,
+      price: 100.00,
+    },
+  ],
+};
